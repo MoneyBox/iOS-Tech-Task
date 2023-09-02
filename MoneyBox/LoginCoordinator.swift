a061b2b0998a0cc507f58ffc93bdfe9d4b45ea8b
Dave <8bitdave@proton.me>
2023-09-02 22:21:47 +0100

    MBT-01 Add login coordinator

--- MoneyBox/LoginCoordinator.swift
+++ MoneyBox/LoginCoordinator.swift
@@ -5,4 +5,40 @@
 //  Created by David Gray on 02/09/2023.
 //
 
-import Foundation
+import Networking
+import UIKit
+
+final class LoginCoordinator: Coordinator {
+    
+    // MARK: - Properties
+    var childCoordinators: [Coordinator] = []
+    var navigationController: UINavigationController
+    
+    private var networkService: DataProviderLogic
+    
+    var userLoggedIn: ((String) -> Void)?
+ 
+    // MARK: - Init
+    init(navigationController: UINavigationController, networkService: DataProviderLogic) {
+        self.navigationController = navigationController
+        self.networkService = networkService
+    }
+    
+    func start() {
+        let viewModel = LoginViewModel(networkService: networkService)
+        
+        viewModel.loginAction = { [weak self] user in
+            self?.navigateToAccounts(user: user)
+        }
+        
+        let viewController = LoginViewController(loginViewModel:
+        viewModel)
+
+        navigationController.pushViewController(viewController, animated: false)
+    }
+    
+    private func navigateToAccounts(user: String) {
+        userLoggedIn?(user)
+    }
+    
+}
